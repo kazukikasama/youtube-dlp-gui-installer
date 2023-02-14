23d5435b228e1991876be5095b2e7c5efc382cb0
kazukikasama <53799401+kazukikasama@users.noreply.github.com>
2023-02-14 03:21:43 -0500

    now include aria2 and installer is 20 times faster
    now you can check Aria2 in yt-lp-gui Options.
    Installer is more faster

--- yt-dlp-gui-installer.ps1
+++ yt-dlp-gui-installer.ps1
@@ -4,10 +4,14 @@
 #make dir in current user appdata
 mkdir $Env:LOCALAPPDATA\yt-dlp-gui
 
-#Download yt-dlp, ffmpeg and yt-dlp-gui from official sites current releases
-Invoke-WebRequest https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe  -OutFile $Env:LOCALAPPDATA\yt-dlp-gui\yt-dlp.exe
-Invoke-WebRequest https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -OutFile $Env:LOCALAPPDATA\yt-dlp-gui\ffmpeg.zip
-Invoke-WebRequest https://github.com/kannagi0303/yt-dlp-gui/releases/latest/download/yt-dlp-gui.exe -OutFile $Env:LOCALAPPDATA\yt-dlp-gui\yt-dlp-gui.exe
+#Download aria2, yt-dlp, ffmpeg and yt-dlp-gui from official sites current releases
+Invoke-WebRequest https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-32bit-build1.zip -OutFile $Env:LOCALAPPDATA\yt-dlp-gui\aria2.zip
+Expand-Archive $Env:LOCALAPPDATA\yt-dlp-gui\aria2.zip -DestinationPath $Env:LOCALAPPDATA\yt-dlp-gui\
+Copy-Item -Path "$Env:LOCALAPPDATA\yt-dlp-gui\aria2-1.36.0-win-32bit-build1\aria2c.exe" -Destination "$Env:LOCALAPPDATA\yt-dlp-gui"
+
+& $Env:LOCALAPPDATA\yt-dlp-gui\aria2c.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -d $Env:LOCALAPPDATA\yt-dlp-gui\ -o yt-dlp.exe
+& $Env:LOCALAPPDATA\yt-dlp-gui\aria2c.exe https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip -d $Env:LOCALAPPDATA\yt-dlp-gui\ -o ffmpeg.zip
+& $Env:LOCALAPPDATA\yt-dlp-gui\aria2c.exe https://github.com/kannagi0303/yt-dlp-gui/releases/latest/download/yt-dlp-gui.exe -d $Env:LOCALAPPDATA\yt-dlp-gui\ -o yt-dlp-gui.exe
 
 #extract ffmpeg
 Expand-Archive $Env:LOCALAPPDATA\yt-dlp-gui\ffmpeg.zip -DestinationPath $Env:LOCALAPPDATA\yt-dlp-gui\
@@ -16,6 +20,8 @@ Expand-Archive $Env:LOCALAPPDATA\yt-dlp-gui\ffmpeg.zip -DestinationPath $Env:LOC
 Copy-Item -Path "$Env:LOCALAPPDATA\yt-dlp-gui\ffmpeg-master-latest-win64-gpl\bin\*" -Destination "$Env:LOCALAPPDATA\yt-dlp-gui"
 remove-item -fo $Env:LOCALAPPDATA\yt-dlp-gui\ffmpeg-master-latest-win64-gpl -recurse
 remove-item -fo $Env:LOCALAPPDATA\yt-dlp-gui\ffmpeg.zip -recurse
+remove-item -fo $Env:LOCALAPPDATA\yt-dlp-gui\aria2-1.36.0-win-32bit-build1 -recurse
+remove-item -fo $Env:LOCALAPPDATA\yt-dlp-gui\aria2.zip -recurse
 
 #make shorcut in Destop
 $desktop = [Environment]::GetFolderPath("Desktop")
