68f36a823d411c16cd231090061715e49986420b
kazukikasama <53799401+kazukikasama@users.noreply.github.com>
2023-02-19 02:33:12 -0500

    add uninstaller for Control Panel

--- yt-dlp-gui-uninstaller.ps1
+++ yt-dlp-gui-uninstaller.ps1
@@ -2,3 +2,4 @@ $desktop = [Environment]::GetFolderPath("Desktop")
 remove-item -fo $Env:LOCALAPPDATA\yt-dlp-gui -recurse
 remove-item -fo "$desktop\Youtube Dowloader P.lnk" -recurse
 remove-item -fo ("$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Youtube Dowloader P.lnk") -recurse
+Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\yt-dlp-gui" -Recurse
