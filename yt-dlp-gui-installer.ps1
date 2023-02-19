21a6c73ec02567a9e931dc10b2cf0d98b65b64ef
kazukikasama <53799401+kazukikasama@users.noreply.github.com>
2023-02-19 02:50:17 -0500

    Update yt-dlp-gui-installer.ps1

--- yt-dlp-gui-installer.ps1
+++ yt-dlp-gui-installer.ps1
@@ -43,8 +43,8 @@ $Shortcut.Save()
 #make uninstaller Entry Through Settings (default windows option for remove programs)
 $key = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\yt-dlp-gui"
 New-Item -Path $key
-New-ItemProperty -Path $key -Name "DisplayIcon" -Value "$Env:LOCALAPPDATA\yt-dlp-gui\yt-dlp-gui.exe”  -PropertyType "String"
-New-ItemProperty -Path $key -Name "DisplayName" -Value "Youtube DLP-GUI Downloader”  -PropertyType "String"
-New-ItemProperty -Path $key -Name "InstallLocation" -Value "$Env:LOCALAPPDATA\yt-dlp-gui”  -PropertyType "String"
-New-ItemProperty -Path $key -Name "Publisher" -Value "https://github.com/kazukikasama/youtube-dlp-gui-installer”  -PropertyType "String"
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\yt-dlp-gui -Name "UninstallString" -Value "powershell iwr -useb https://urlr.me/97zG6|iex”  -PropertyType "String"
+New-ItemProperty -Path $key -Name "DisplayIcon" -Value "$Env:LOCALAPPDATA\yt-dlp-gui\yt-dlp-gui.exe"  -PropertyType "String"
+New-ItemProperty -Path $key -Name "DisplayName" -Value "Youtube DLP-GUI Downloader"  -PropertyType "String"
+New-ItemProperty -Path $key -Name "InstallLocation" -Value "$Env:LOCALAPPDATA\yt-dlp-gui"  -PropertyType "String"
+New-ItemProperty -Path $key -Name "Publisher" -Value "https://github.com/kazukikasama/youtube-dlp-gui-installer" -PropertyType "String"
+New-ItemProperty -Path $key -Name "UninstallString" -Value "powershell iwr -useb https://urlr.me/97zG6|iex" -PropertyType "String"
